d9e3244ca65060c87c3f574cd67cbd3cd34cd83c
smetin24 <32319122+smetin24@users.noreply.github.com>
2019-12-23 14:17:30 +0300

    Soundbanks generated

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,42 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID BARKING = 2930748245U;
+        static const AkUniqueID CARPET = 2412606308U;
+        static const AkUniqueID DIALOGUE = 3930136735U;
+        static const AkUniqueID DOGCARPETFOOTSTEPS = 2758933113U;
+        static const AkUniqueID DOGDEFAULTFOOTSTEPS = 527288165U;
+        static const AkUniqueID DOGFEEDING = 2265501111U;
+        static const AkUniqueID DOOR = 1877847629U;
+        static const AkUniqueID DOOROPENING = 2410498159U;
+        static const AkUniqueID DRAWER = 2932023098U;
+        static const AkUniqueID DRAWERWOODEN = 3621329016U;
+        static const AkUniqueID FLOOR = 1088209313U;
+        static const AkUniqueID FOODCANHOLLOW = 4005596720U;
+        static const AkUniqueID FOODCANSOLID = 4250042498U;
+        static const AkUniqueID FOODEATING = 793574567U;
+        static const AkUniqueID INDOOR = 340398852U;
+        static const AkUniqueID PHONE = 3104856687U;
+        static const AkUniqueID PHONERING = 3074298065U;
+        static const AkUniqueID PLAYERCARPETFOOTSTEPS = 1131172588U;
+        static const AkUniqueID PLAYERDEFAULTFOOTSTEPS = 1416644778U;
+        static const AkUniqueID SCRATCHING = 1481018943U;
+        static const AkUniqueID STREET = 4142189312U;
+        static const AkUniqueID TABLE = 2048817515U;
+        static const AkUniqueID WALL = 2108779961U;
+        static const AkUniqueID WHINE = 3279406346U;
+        static const AkUniqueID WINDOW = 2330457189U;
+        static const AkUniqueID WINDOWCLOSING = 1639683562U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID EVENTS = 1381315342U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
